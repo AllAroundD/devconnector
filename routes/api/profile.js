bcd14102ba4e457423fd462eae3f5081b33f3d5e
AllAroundD <AllAroundDWeb@gmail.com>
2020-12-15 22:20:05 -0500

    updated profile.js to get all profiles and by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -105,4 +105,40 @@ async (req, res) => {
     }
 })
 
+// @route   GET api/profile/
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profiles)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+        
+    }
+})
+
+// @route   GET api/profile/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar'])
+        if (!profile) {
+            return res.status(400).json({ msg: 'Profile not found' })
+        }
+        
+        res.json(profile)
+    } catch (err) {
+        if(err.kind == 'ObjectId') {
+            console.error('Profile not found')
+            return res.status(400).json({ msg: 'Profile not found' })
+        }
+        console.error(err.message)
+        res.status(500).send('Server Error')
+        
+    }
+})
+
 module.exports = router
