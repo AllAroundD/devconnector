ea0700ef3c2f1501510a72b7f948afc627c25bbd
AllAroundD <AllAroundDWeb@gmail.com>
2020-12-20 22:10:47 -0500

    added user authentication

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 import { setAlert } from '../../actions/alert'
+import { register } from '../../actions/auth'
 import PropTypes from 'prop-types'
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -22,8 +23,13 @@ const Register = ({ setAlert }) => {
         if(password !== password2) {
             setAlert('Passwords do not match', 'danger')
         } else {
-            console.log('SUCCESS')
+            register({ name, email, password })
         }
+    }
+
+    // Redirect if logged in
+    if(isAuthenticated) {
+        return <Redirect to='/dashboard' />
     }
 
     return (
@@ -38,7 +44,7 @@ const Register = ({ setAlert }) => {
                     name="name" 
                     value={name} 
                     onChange={e => onChange(e)}
-                    required 
+                    // required 
                     />
                 </div>
                 <div className="form-group">
@@ -48,7 +54,7 @@ const Register = ({ setAlert }) => {
                         name="email"
                         value={email}
                         onChange={e => onChange(e)}
-                        required 
+                        // required 
                         />
                     <small className="form-text">
                         This site uses Gravatar so if you want a profile image, use a
@@ -62,7 +68,7 @@ const Register = ({ setAlert }) => {
                     name="password"
                     value={password}
                     onChange={e => onChange(e)}
-                    minLength="6"
+                    // minLength="6"
                 />
                 </div>
                 <div className="form-group">
@@ -72,7 +78,7 @@ const Register = ({ setAlert }) => {
                     name="password2"
                     value={password2}
                     onChange={e => onChange(e)}
-                    minLength="6"
+                    // minLength="6"
                 />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Register" />
@@ -85,7 +91,13 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,            // ptb (emmett)
 }
 
-export default connect(null, { setAlert })(Register)
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, { setAlert, register })(Register)
