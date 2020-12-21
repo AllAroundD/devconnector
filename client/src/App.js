38c33f9373ae3dd43c3e0155712878fded02f392
AllAroundD <AllAroundDWeb@gmail.com>
2020-12-20 22:10:47 -0500

    added user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
@@ -8,26 +8,38 @@ import Alert from './components/layout/Alert'
 // Redux
 import { Provider } from 'react-redux'
 import store from './store'
+import { loadUser } from './actions/auth'
+import setAuthToken from './utils/setAuthToken'
 
 import './App.css';
 
+if(localStorage.token) {
+  // set the token with the header
+  setAuthToken(localStorage.token)
+}
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className="container">
-          <Alert />
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-)
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser())
+  }, [])   // only runs at initial load
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className="container">
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+    )
+  }
 
 export default App;
