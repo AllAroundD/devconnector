4000866828de9472d58377a3f12c3931e83bb14f
AllAroundD <AllAroundDWeb@gmail.com>
2020-12-28 16:08:44 -0500

    tidied up some files

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -112,6 +112,7 @@ router.put('/like/:id', auth, async (req, res) => {
 
         // Check if the post has already been liked
         if (post.likes.some(like => like.user.toString() === req.user.id)) {
+            console.log('msg: Post already liked')
             return res.status(400).json({ msg: 'Post already liked' });
         }
         // if(post.likes.filter(like => 
@@ -138,20 +139,28 @@ router.put('/unlike/:id', auth, async (req, res) => {
         const post = await Post.findById(req.params.id)
 
         // Check if the post has already been liked
-        if(post.likes.filter(like => 
-            like.user.toString() === req.user.id).length === 0) {
-            return res.status(400).json({ msg: 'Post has not yet been liked' })
+        // if(post.likes.filter(like => 
+            // like.user.toString() === req.user.id).length === 0) {
+
+        if (!post.likes.some(like => like.user.toString() === req.user.id)) {
+            console.log('msg: Post has not yet been liked')
+            return res.status(400).json({ msg: 'Post has not yet been liked' });
         }
 
-        // Get remove index
-        const removeIndex = post.likes.map(like => 
-            like.user.toString()).indexOf(req.user.id)
+        // // Get remove index
+        // const removeIndex = post.likes.map(like => 
+        //     like.user.toString()).indexOf(req.user.id)
 
-        post.likes.splice(removeIndex, 1)
+        // post.likes.splice(removeIndex, 1)
+
+        // remove the like
+        post.likes = post.likes.filter(
+            ({ user }) => user.toString() !== req.user.id
+        );
 
         await post.save()
 
-        res.json(post.likes)
+        return res.json(post.likes)
     } catch (err) {
         console.error(err.message)
         res.status(500).send('Server Error')
