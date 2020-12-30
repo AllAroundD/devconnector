c22c2ff6321dd997fb61663e622db83ae3862dca
AllAroundD <AllAroundDWeb@gmail.com>
2020-12-30 17:53:01 -0500

    added single post display

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import PostItem from '../posts/PostItem'
 import { getPost } from '../../actions/post'
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
@@ -9,11 +11,16 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         getPost(match.params.id)
     }, [getPost, match.params.id])
 
-    return (
-        <div>
-            post
-        </div>
-    )
+    return (loading || post === null ? (
+        <Spinner /> 
+    ) : (
+        <Fragment>
+            <Link to='/posts' className='btn'>
+                Back To Posts
+                </Link>
+            <PostItem post={post} showActions={false} />
+        </Fragment>
+    ))
 }
 
 Post.propTypes = {
