210c7b2b1243e61a10d8c72ce4326f36eeb818d0
AllAroundD <AllAroundDWeb@gmail.com>
2020-12-31 11:59:41 -0500

    Prepared for deployment

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -24,7 +24,7 @@ const CommentItem = ({
           </div>
           <div>
             <p className="my-1">{text}</p>
-             <p className="post-date">
+            <p className="post-date">
                 Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
             </p>
             {!auth.loading && user === auth.user._id && (
