e25092e62074161a7532fa5fba19eeefed8dee2c
AllAroundD <AllAroundDWeb@gmail.com>
2021-01-04 12:47:02 -0500

    added class to dashboard to fix when dark mode

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -36,7 +36,7 @@ const Dashboard = ({
                 </Fragment>
             ) : (
                 <Fragment>
-                    <p>You have not yet setup a profile, please add some info</p>
+                    <p className="my-1"><span>You have not yet setup a profile, please add some info</span></p>
                     <Link to='/create-profile' className="btn btn-primary my-1">
                         Create Profile
                     </Link>
